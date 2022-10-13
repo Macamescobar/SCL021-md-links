3eee81e43894dd93e3e75e2c6e41a754bc7cddc7
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-10-13 08:38:30 -0300

    Actualizacion funcion que extrae archivos.md

---
+++ prueba/README.md
@@ -0,0 +1,8 @@
+
+http://patriciaemiguel.com/conceptos/2020/04/24/operador-ternario.html
+
+https://www.woodwardspanish.com/lesson/rutinas-diarias-daily-routines-spanish/
+
+https://regexr.com/
+
+https://dev.to/duxtech/4-metodos-importantes-del-modulo-path-en-node-js-ai2
--- src/api.js
+++ src/api.js
@@ -1,42 +1,69 @@
 #!/usr/bin/env node
 
 //Import the path module
-const path = require('path');
+const path = require("path");
 // File system module
-const fs = require('fs');
+const fs = require("fs");
 
 // Modulo, solicitud HTTP
 //const fetch = require("node-fetch");
 
-// 1. Obteniendo la ruta
+////////// 1. Returns an array containing the arguments,
+// the second element is the path for the js file.
 const arguments = process.argv[2];
-console.log(process.argv)
+// console.log(process.argv);
 
-////////// 2 Función que comprueba si existe la ruta 
-const validatePath = (route) => fs.existsSync(route)
+////////// 2. Verifica si existe el archivo en la ruta dada o no (retorna true o false)
+const validatePath = (route) => fs.existsSync(route);
 
-////////// 3.Funcion que comprueba si la ruta es absoluta
-const convertToAbsolute = (route) => path.resolve(route);
+////////// 3. Retorna un string con la ruta absoluta
+const absolutePath = (route) => path.resolve(route);
 
-////////// 4.Funcion que verifica si es directorio
+////////// 4.Verifica si la ruta es archivo o directorio 
 const itsDirectory = (route) => fs.statSync(route).isDirectory();
 
-///////// 5. Funcion que verdifica si tiene archivos md
-const mdExtension = (route) => path.extname(route);
+///////// 5. Retorna archivos con .md extension
+const mdExtension = (routes) => {
+  return routes.filter((route) => {
+    console.log('extension',path.extname(route));
+  return  path.extname(route) === ".md"})
 
-///////// 6. Funcion que lee los archivoS
+}
+
+///////// 6. Leer el archivo y retornar su contenido
 const readFile = (route) => fs.readFileSync(route, { encoding: "utf-8", flag: "r" });
 
+
+//////////// 6.Funcion que extrae archivos .md
+function searchFileMd(route) {
+  let allFilesMd = [];
+  if(itsDirectory(route)) {
+    //Leer el contenido del directorio y retornar una array de objetos que contienen los archivos en el directorio.
+    const readDirectory = fs.readdirSync(route);
+    console.log({readDirectory});
+    const filesMd = mdExtension(readDirectory)
+    filesMd.forEach((file) => {
+      const dataFile = readFile(file)
+      console.log({dataFile});
+    })
+  }
+  return allFilesMd;
+}
+
+
+
+
+
+module.exports = { validatePath };
 
 
 //Probando
 const temporal = () => {
-    try {
-      const result =  validatePath(arguments);
-        console.log({result});
-    } catch (error) {
-        console.error({error});
-    }
-}
+  try {
+    const result = searchFileMd(arguments);
+    console.log({ result });
+  } catch (error) {
+    console.error({ error });
+  }
+};
 temporal();
-module.exports = { validatePath};
